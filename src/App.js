87ad2565d99248e9f04ad4a9fa3a0f32988bd014
Vaibhav Srivastava <86321058+VaibhavJeet@users.noreply.github.com>
2023-02-26 15:16:23 +0530

    Chat-bot

--- src/App.js
+++ src/App.js
@@ -1,23 +1,321 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import image from "./img/bot_image.png";
+import { useState, useRef, useMemo  } from "react";
+import { IconContext } from "react-icons";
+import { BsChevronDown, BsChatLeftFill} from 'react-icons/bs';
+import { BiMicrophoneOff, BiMicrophone } from 'react-icons/bi'
 
 function App() {
+  // function TodoList({ todos, tab, theme }) {
+  //   const visibleTodos = useMemo(() => filterTodos(todos, tab), [todos, tab]);
+  // }
+  const humanMessage = useRef();
+  const botmessage = useRef();
+  const input = useRef();
+
+  const date = new Date();
+  const hours = date.getHours();
+  const seconds = date.getSeconds();
+  const day = date.getDay();
+  const month = date.getMonth();
+  const year = date.getFullYear();
+  const [chatScreen, setChatScreen] = useState(false);
+  const chatScreentoggle = () => {
+    console.log("clicked")
+    setChatScreen(true)
+  }
+  const [secondScreen, setSecondScreen] = useState(false);
+  const secondScreentoggle = () => {
+    console.log("hello")
+    setSecondScreen(true)
+    document.querySelector('.chat-bot-first-screen-chat-bot-wrapper').style.display = "none";
+  }
+
+  const [microphoneIcon, setmicrophoneIcon] = useState(false);
+
+
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const [time, setTime] = useState(`${hours}:${seconds}`); //using the useState hook to get the data from the local time and set it to the time variable
+  const [dateTime, setDateTime] = useState(
+    `${days[day]}, ${months[month]} ${year}`
+  ); //using the useState hook to get the data from the local date and set it to the dateTime variable
+
+  const checkStatus = (e) => {
+    let isActive = true;
+    if (dateTime === "Thursday, Aug 13 2023") {
+      //if the dateTime is Thursday, 13 Aug 2023, the bot will be inactive
+      isActive = false;
+    }
+    const status = document.querySelector(".status");
+    // selecting the status class
+    if (isActive === true) {
+      //if the bot is active
+      status.innerHTML = "Active";
+      status.style.color = "green";
+    } else {
+      status.innerHTML = "Not Active";
+      status.style.color = "red";
+    }
+  };
+  const handleInput = () => {
+    const botMessage = document.querySelector("#message1");
+    const userMessage = document.querySelector("#message2");
+    const inputRef = input.current;
+    const getHumanMessage = humanMessage.current;
+    const getBotMessage = botmessage.current;
+
+    let badwords = ["fuck|bad|stupid|useless|bitch|crazy|nonsense"];
+    let words = new RegExp(badwords);
+    if (words.test(document.querySelector("#input").value)) {
+      // if the input contains bad words
+      getBotMessage.innerText = "Typing...";
+      setTimeout(() => {
+        getBotMessage.innerText = "Please do not use bad words"; // display the message
+        inputRef.value = ""; // clear the input
+      }, 2000);
+    }
+    let welcome = [
+      "hi|hello|Hello|hey|sup|yo|wassup|whats up|howdy|greetings|good morning|good afternoon|good evening",
+    ];
+    let words2 = new RegExp(welcome);
+    if (words2.test(document.querySelector("#input").value)) {
+      const status = document.querySelector(".status");
+      // if the input contains welcome words
+      getBotMessage.innerText = "Typing...";
+      setTimeout(() => {
+        getBotMessage.innerText = "Hello There how are you doing today?"; // display the message
+        status.innerText = "Active";
+        status.style.color = "green";
+        inputRef.value = ""; // clear the input
+      }, 2000);
+    }
+    let bye = ["bye|Bye|goodbye|see you later|cya|goodnight|goodbye"];
+    let words3 = new RegExp(bye);
+    if (words3.test(document.querySelector("#input").value)) {
+      const status = document.querySelector(".status");
+      getBotMessage.innerText = "Typing...";
+      setTimeout(() => {
+        getBotMessage.innerText = "Bye, have a nice day";
+        inputRef.value = ""; // clear the input
+      }, 2000);
+      setTimeout(() => {
+        status.innerText = "Not active";
+        status.style.color = "red";
+      }, 5000);
+    }
+    let thanks = [
+      "Thanks|thanks|thank you|thank you very much|Thank you very much",
+    ];
+    let words4 = new RegExp(thanks);
+    if (words4.test(document.querySelector("#input").value)) {
+      getBotMessage.innerText = "Typing...";
+      setTimeout(() => {
+        getBotMessage.innerText = "You are welcome";
+        inputRef.value = ""; // clear the input
+      }, 2000);
+    }
+    let how = [
+      "How are you|how are you doing|how are you doing today|how are you doing today|How are you|how are you",
+    ];
+    let words5 = new RegExp(how);
+    if (words5.test(document.querySelector("#input").value)) {
+      const status = document.querySelector(".status");
+      getBotMessage.innerText = "Typing...";
+      setTimeout(() => {
+        getBotMessage.innerText = "I am fine, thank you";
+        status.innerText = "Active";
+        status.style.color = "green";
+        inputRef.value = ""; // clear the input
+      }, 2000);
+    }
+    let good = [
+      "That's good|Sound nice|that sounds awesome|that sounds great|Great|great|sounds great|that's sounds good|Nice|nice",
+    ];
+    let words6 = new RegExp(good);
+    if (words6.test(document.querySelector("#input").value)) {
+      getBotMessage.innerText = "Typing...";
+      setTimeout(() => {
+        getBotMessage.innerText = "😁";
+        inputRef.value = ""; // clear the input
+      }, 1000);
+    }
+
+    let response = [
+      "I'm fine|I am fine|I am fine today|I am fine today|i'm fine|i'm great|I'm fine|I'm great|I'm good|i'm good|great|Great",
+    ];
+    let words7 = new RegExp(response);
+    if (words7.test(document.querySelector("#input").value)) {
+      getBotMessage.innerText = "Typing...";
+      setTimeout(() => {
+        getBotMessage.innerText = "That is good";
+        inputRef.value = ""; // clear the input
+      }, 2000);
+    }
+
+    let nameAsk = [
+      "What's your name|what's your name|What is your name|what is your name",
+    ];
+    let words8 = new RegExp(nameAsk);
+    if (words8.test(document.querySelector("#input").value)) {
+      getBotMessage.innerText = "Typing...";
+      setTimeout(() => {
+        getBotMessage.innerText = "My name is Bot";
+        inputRef.value = ""; // clear the input
+      }, 2000);
+    }
+
+    let owner = [
+      "Who is the owner|who is the owner|Who is the owner of this bot|who is the owner of this bot|Who made you|who made you|Who is your maker|Who made you|who is your maker|who is your owner|Who is your owner",
+    ];
+    let words9 = new RegExp(owner);
+    if (words9.test(document.querySelector("#input").value)) {
+      getBotMessage.innerText = "Typing...";
+      setTimeout(() => {
+        getBotMessage.innerText = "The owner of this bot is a human";
+        inputRef.value = ""; // clear the input
+      }, 2000);
+    }
+
+    let owner2 = [
+      "Who's human|who's human|Who is human|who is human",
+    ];
+    let words10 = new RegExp(owner2);
+    if (words10.test(document.querySelector("#input").value)) {
+      getBotMessage.innerText = "Typing...";
+      setTimeout(() => {
+        getBotMessage.innerText =
+          "You";
+        inputRef.value = ""; // clear the input
+      }, 2000);
+    }
+
+    let ageAsk = [
+      "What's your age|what's your age|What is your age|what is your age|How old are you|how old are you",
+    ]; //adding the age-question
+    let words11 = new RegExp(ageAsk);
+    if (words11.test(document.querySelector("#input").value)) {
+      // if the input contains some question
+      getBotMessage.innerText = "Typing...";
+      setTimeout(() => {
+        getBotMessage.innerText = "I am 1 day old";
+        inputRef.value = ""; // clear the input
+      }, 2000);
+    }
+    getHumanMessage.innerText = inputRef.value; // display the message
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Chat-bot-app" onLoad={checkStatus}>
+      <div className={chatScreen === true ? "chat-bot-icon-hide " : "chat-bot-icon"} onClick={() => chatScreentoggle()}>
+      <IconContext.Provider value={{ className: 'chat-react-icons' }}>
+        <BsChatLeftFill />
+      </IconContext.Provider>
+      </div>
+      <div className={chatScreen === false ? "chat-bot-first-screen-chat-bot-wrapper-hide" : "chat-bot-first-screen-chat-bot-wrapper"}>
+        <div className="chat-bot-first-screen">
+          <div className="chat-bot-first-screen-title">
+            Let’s chat!
+          </div>
+          <div className="chat-bot-first-screen-text">
+            <p>Hi !👋 How Can I help you?</p>
+          </div>
+          <div className="chat-bot-first-screen-main">
+            <div className="chat-bot-btn-div">
+              <div className="chat-bot-btn-group">
+                <button> Flip List Reports</button>
+                <button >Antwalk </button>
+                <button >Statistics</button>
+                <button onClick={() => secondScreentoggle()}>Start Chat</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={secondScreen === false ? "chat-bot-wrapper-hide" : "chat-bot-wrapper"}>
+        <div className="content">
+          <div className="header">
+            <div className="img">
+              <img src={image} alt="" />
+            </div>
+            <div className="right">
+              <div className="name">ChatBot</div>
+              <div className="status">Active</div>
+            </div>
+            <div className="chat-bot-drop-down">
+                <div id="menu-wrap">
+                  <input type="checkbox" className="toggler" />
+                  <BsChevronDown/>
+                  <div className="menu">
+                    <div>
+                      <ul>
+                        <li><a href="#" className="link">Minimize</a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+            </div>
+          </div>
+          <div className="main">
+            <div className="main_content">
+              <div className="messages">
+                <div
+                  className="bot-message"
+                  id="message1"
+                  ref={botmessage}
+                ></div>
+                <div
+                  className="human-message"
+                  id="message2"
+                  ref={humanMessage}
+                ></div>
+              </div>
+            </div>
+          </div>
+          <div className="bottom">
+            <div className="btm">
+              <div className="input">
+                <input
+                  type="text"
+                  id="input"
+                  placeholder="Enter your message"
+                  ref={input}
+                />
+              </div>
+              <div className="mikebtn">
+                <button onClick={() => setmicrophoneIcon(!microphoneIcon)}>
+                  {microphoneIcon ? <BiMicrophoneOff/> : <BiMicrophone/> }
+                </button>
+              </div>
+              <div className="btn">
+                <button onClick={() => handleInput()}>
+                  <i className="fas fa-paper-plane"></i>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
