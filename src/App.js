b612df8b6a43f3eed66587d5ceb4c6edf639e251
Vaibhav Srivastava <86321058+VaibhavJeet@users.noreply.github.com>
2023-03-01 00:08:15 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -3,103 +3,227 @@ import "./App.css";
 import axios from 'axios';
 import image from "./img/bot_image.png";
 import { IconContext } from "react-icons";
-import { BsChevronDown, BsChatLeftFill, BsChatLeft } from 'react-icons/bs';
-import { BiMicrophoneOff, BiMicrophone } from 'react-icons/bi';
+import { BsChevronDown, BsChatLeftFill, BsChatLeft, BsMicMute, BsMic } from 'react-icons/bs';
 import { RiSendPlaneFill } from 'react-icons/ri'
-import ResponsiveVoice from 'responsivevoice';
+
+
+
+
 
 
 function App() {
+  //for displaying top asked questions
+  const [frequentlyAskedQuestions, setFrequentlyAskedQuestions] = useState({
+  });
+  const FAQ_COUNT = 5; // display top 5 frequently asked questions
 
+  const mostFrequentlyAskedQuestions = Object.entries(frequentlyAskedQuestions)
+    .sort((a, b) => b[1] - a[1]) // sort by count (descending order)
+    .slice(0, FAQ_COUNT) // take the top N questions
+    .map(([question, count]) => `${question} (${count} times)`)
+    .join('\n');
+
+  console.log('Most frequently asked questions:\n' + mostFrequentlyAskedQuestions);
+  //top asked questions end
+
+
+  ////----------------------------------------------------------------------------------------\\\\\  
+
+
+  //for chat icon visibility on click
   const [chatIcon, setChatIcon] = useState(false);
+  //for chat icon visibility on click end
 
+
+  ////----------------------------------------------------------------------------------------\\\\\  
+
+  //for giving icons custom css
   const chatIconElement = useMemo(() => {
     return (
-      <IconContext.Provider value={{ className: 'chat-react-icons' }}>
+      <IconContext.Provider value={{ className: "chat-react-icons" }}>
         {chatIcon ? <BsChatLeft /> : <BsChatLeftFill />}
       </IconContext.Provider>
     );
   }, [chatIcon]);
+  //for giving icons custom css end
+
+
+  ////----------------------------------------------------------------------------------------\\\\\   
+
+
+  //for toggling between different screens of chatbot
+  const [chatScreen, setChatScreen] = useState(false);
+  const chatScreentoggle = () => {
+    setChatScreen(true);
+  };
+  const [secondScreen, setSecondScreen] = useState(false);
+  const secondScreentoggle = () => {
+    setSecondScreen(true);
+    document.querySelector(".chat-bot-first-screen-chat-bot-wrapper").style.display =
+      "none";
+  };
+  //for toggling between different screens of chatbot end
+
+  ////----------------------------------------------------------------------------------------\\\\\
+
+
+  //for handling user message and bot reply 
   const chatMessagesRef = useRef(null);
   const humanMessage = useRef();
   const botmessage = useRef();
   const input = useRef();
 
-  const [chatScreen, setChatScreen] = useState(false);
-  const chatScreentoggle = () => {
-    setChatScreen(true)
-  }
-  const [secondScreen, setSecondScreen] = useState(false);
-  const secondScreentoggle = () => {
-    setSecondScreen(true)
-    document.querySelector('.chat-bot-first-screen-chat-bot-wrapper').style.display = "none";
-  }
-
-  //Speech Start
-  const [inputText, setInputText] = useState("");
   const [microphoneIcon, setMicrophoneIcon] = useState(false);
-  const [micPermission, setMicPermission] = useState(false);
-  const recognition = useRef(null);
+  const recognitionRef = useRef(null);
 
   const handleMicrophone = () => {
     setMicrophoneIcon(!microphoneIcon);
-    if (!microphoneIcon) {
-      startRecognition();
-    } else {
-      stopRecognition();
+
+    const recognition = new window.webkitSpeechRecognition();
+    recognition.continuous = true; // keep listening until stopped
+    recognition.interimResults = true; // return results as they become available
+    recognition.lang = 'en-US'; // set the language to English (US)
+    recognition.onstart = () => {
+      console.log('Listening...'); // log when speech recognition starts
+    };
+    recognition.onresult = (event) => {
+      const speechToText = event.results[0][0].transcript;
+      console.log(speechToText); // log the recognized text to the console
+      input.current.value = speechToText;
+      // use the recognized text to update your application state or perform any other action
+    };
+    recognition.onerror = (event) => {
+      console.error(event.error); // log any errors that occur during speech recognition
+    };
+    recognitionRef.current = recognition;
+    recognition.start();
+  };
+
+  const stopSpeechRecognition = () => {
+    if (recognitionRef.current) {
+      console.log('Stopped listening.'); // log when speech recognition stops
+      recognitionRef.current.stop();
     }
   };
 
-  const startRecognition = () => {
-  navigator.mediaDevices.getUserMedia({ audio: true })
-  .then(() => {
-    setMicPermission(true);
-    recognition.current.start();
-  })
-  .catch((error) => {
-    console.error(error);
-    setMicPermission(false);
-  });
-
-    if (window.hasOwnProperty("webkitSpeechRecognition")) {
-      recognition.current = new window.webkitSpeechRecognition();
-      recognition.current.continuous = true;
-      recognition.current.interimResults = true;
-      recognition.current.lang = "en-US";
-      recognition.current.start();
-      recognition.current.onresult = function (e) {
-        var resultIndex = e.resultIndex;
-        var transcript = e.results[resultIndex][0].transcript;
-        setInputText((prevText) => prevText + transcript);
+  const speak = (text) => {
+    if ('speechSynthesis' in window && microphoneIcon) {
+      stopSpeechRecognition();
+      const utterance = new SpeechSynthesisUtterance(text);
+      utterance.lang = 'en-IN'; // set the language
+      utterance.volume = 0.5; // set the volume (0 to 1)
+      utterance.pitch = 1; // set the pitch (0 to 2)
+      utterance.rate = 1.2; // set the rate (0.1 to 10)
+      utterance.onend = () => {
+        recognitionRef.current.start();
       };
+      window.speechSynthesis.speak(utterance);
+    } else {
+      console.log('Speech synthesis is not supported in your browser.');
     }
   };
 
-  const stopRecognition = () => {
-    if (recognition.current) {
-      recognition.current.stop();
-    }
-  };
+  const [chatTime, setChatTime] = useState("");
 
-  const speak1 = (text) => {
-    if (!micPermission) {
-      return;
-    }
+  const handleLinkBtn = () => {
+    const botMessagebtnEl = document.createElement("div");
+    botMessagebtnEl.className = "bot-message";
+    botMessagebtnEl.innerHTML = `<a href="https://www.youtube.com
+    ">Click this link to go somewhere!!</a>`;
+    chatMessagesRef.current.appendChild(botMessagebtnEl);
+    chatMessagesRef.current.scrollTop = chatMessagesRef.current.scrollHeight;
+  }
 
-    if (recognition.current) {
-      recognition.current.stop();
-    }
+  useEffect(() => {
+    const botHtml = "Hi, I am your assistant. Please ask a question or choose from the list below";
+    botmessage.current.innerHTML = botHtml;
+  }, []);
 
-    ResponsiveVoice.speak(text, "Hindi Male", {
-      onend: () => {
-        if (microphoneIcon) {
-          startRecognition();
+  const API_KEY = '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';
+
+  const callBot = async (msgFrmUser, callback) => {
+    try {
+      const question = msgFrmUser;
+      const model = "text-davinci-003";
+      const modelResponse = await axios.post(
+        "https://api.openai.com/v1/engines/" + model + "/completions",
+        {
+          prompt: question + "?",
+          temperature: 0,
+          max_tokens: 300,
+
+          top_p: 1,
+          n: 1,
+          frequency_penalty: 0,
+          presence_penalty: 0,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${API_KEY}`,
+          },
         }
-      },
+      );
+      // Update frequently asked questions
+      setFrequentlyAskedQuestions((prev) => {
+        const updated = { ...prev };
+        updated[question] = (updated[question] || 0) + 1;
+        return updated;
+      });
+      const botHtml = modelResponse.data.choices[0].text.trim();
+      callback(botHtml);
+    } catch (error) {
+      console.error(error);
+      callback("Sorry, I couldn't understand your question. Please try again.");
+    }
+  };
+
+
+  const handleInputText = () => {
+    const inputText = input.current.value.trim();
+    if (inputText === '') return;
+    const humanMessageEl = document.createElement("div");
+    humanMessageEl.className = "human-message";
+    humanMessageEl.innerHTML = inputText;
+
+    if (inputText.endsWith('?')) { // check if it's a question
+      setFrequentlyAskedQuestions(prev => {
+        const question = inputText.toLowerCase();
+        return {
+          ...prev,
+          [question]: (prev[question] || 0) + 1 // increment count or set to 1
+        };
+      });
+    }
+
+    chatMessagesRef.current.appendChild(humanMessageEl);
+    input.current.value = '';
+    setChatTime(new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }));
+    callBot(inputText, (botResponse) => {
+      const botMessageEl = document.createElement("div");
+      botMessageEl.className = "bot-message";
+      botMessageEl.innerHTML = botResponse;
+      chatMessagesRef.current.appendChild(botMessageEl);
+      chatMessagesRef.current.scrollTop = chatMessagesRef.current.scrollHeight;
+      speak(botResponse);
+      if (botResponse.includes("Click this link")) {
+        setTimeout(handleLinkBtn, 2000);
+      }
     });
   };
-  //Speech End
 
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      handleInputText();
+    }
+  };
+
+  const handleInput = () => {
+    handleInputText();
+  };
+  //for handling user message and bot reply 
+
+  ////----------------------------------------------------------------------------------------\\\\\
 
   //Get Time Start
   function getTime() {
@@ -119,8 +243,6 @@ function App() {
     return time;
   }
 
-  const [chatTime, setChatTime] = useState("");
-
   useEffect(() => {
     // Call getTime() function to get the current time
     const time = getTime();
@@ -130,98 +252,8 @@ function App() {
 
   //Get Time End
 
-  function botmsg() {
-    //
-  }
-  useEffect(() => {
-    const botHtml = "Hi, I am your assistant. Please ask a question or choose from the list below";
-    botmessage.current.innerHTML = botHtml;
-  }, []);
 
-  const API_KEY = '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';
-
-
-  const callBot = async (msgFrmUser, callback) => {
-    try {
-      const question = msgFrmUser;
-      const model = "text-davinci-003";
-      const modelResponse = await axios.post(
-        "https://api.openai.com/v1/engines/" + model + "/completions",
-        {
-          prompt: question + "?",
-          temperature: 0,
-          max_tokens: 300,
-          top_p: 1,
-          frequency_penalty: 0,
-          presence_penalty: 0,
-        },
-        {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${API_KEY}`,
-          },
-        }
-      );
-      const chatbotResponse = modelResponse.data.choices[0].text;
-      callback(chatbotResponse); // call the callback function with chatbot response
-      ///
-      const text = input.current.value;
-      setInputText(text);
-      input.current.value = "";
-      speak1(text)
-      ///
-    } catch (error) {
-      console.error(error);
-      if (error.response && error.response.status === 403) {
-        callback("Sorry, the API key is invalid.");
-      } else {
-        callback("Sorry, there was an error with the chatbot.");
-      }
-    }
-  };
-
-
-  const handleInput = () => {
-    const inputRef = input.current;
-    const { current: getHumanMessage } = humanMessage;
-    const { current: getBotMessage } = botmessage;
-
-    const inputValue = inputRef.value;
-
-    if (getHumanMessage) {
-      // if the input contains words
-      getBotMessage.innerText = "Typing...";
-      callBot(inputValue, (chatbotResponse) => {
-        const botMessageText = chatbotResponse;
-        const botMessageEl = document.createElement("div");
-        botMessageEl.className = "bot-message";
-        botMessageEl.innerText = botMessageText;
-        chatMessagesRef.current.appendChild(botMessageEl);
-        chatMessagesRef.current.scrollTop = chatMessagesRef.current.scrollHeight;
-      });
-      inputRef.value = ""; // clear the input
-    } else {
-      getBotMessage.innerText = "Typing...";
-      setTimeout(() => {
-        const botMessageText = "I'm sorry, I didn't understand.";
-        const botMessageEl = document.createElement("div");
-        botMessageEl.className = "bot-message";
-        botMessageEl.innerText = botMessageText;
-        chatMessagesRef.current.appendChild(botMessageEl);
-        chatMessagesRef.current.scrollTop = chatMessagesRef.current.scrollHeight;
-        inputRef.value = ""; // clear the input
-      }, 2000);
-    }
-    if (inputValue.length > 0) {
-      const humanMessageText = inputValue;
-      const humanMessageEl = document.createElement("div");
-      humanMessageEl.className = "human-message";
-      humanMessageEl.innerText = humanMessageText;
-      chatMessagesRef.current.appendChild(humanMessageEl);
-      chatMessagesRef.current.scrollTop = chatMessagesRef.current.scrollHeight;
-      inputRef.value = "";
-    }
-  };
+  ////----------------------------------------------------------------------------------------\\\\\  
 
   //Reloads ChatApp Start
   const [reload, setReload] = useState(false);
@@ -235,6 +267,7 @@ function App() {
     return <App />;
   }
   //Reload Chat App End
+
 
   return (
     <div className="Chat-bot-app">
@@ -294,8 +327,9 @@ function App() {
           <div className="main">
             <div className="main_content">
               <div className="messages" ref={chatMessagesRef}>
-                <p>Chat started at: {chatTime}</p>
+                <p>{chatTime}</p>
                 <div className="bot-message" id="message1" ref={botmessage}></div>
+                <button onClick={() => handleLinkBtn()} className="bot-message">Click on this button to see something</button>
                 <div id="message2" ref={humanMessage}></div>
               </div>
             </div>
@@ -304,20 +338,15 @@ function App() {
             <div className="btm">
               <div className="input">
                 <input
-                  type="text"
-                  id="input"
-                  placeholder="Enter your message"
                   ref={input}
-                  onKeyPress={(event) => {
-                    if (event.key === "Enter") {
-                      handleInput();
-                    }
-                  }}
+                  type="text"
+                  placeholder="Type your message here"
+                  onKeyPress={handleKeyPress}
                 />
               </div>
               <div className="mikebtn">
                 <button onClick={() => handleMicrophone()}>
-                  {microphoneIcon ? <BiMicrophone/> : <BiMicrophoneOff  />}
+                  {microphoneIcon ? <BsMic /> : <BsMicMute />}
                 </button>
               </div>
               <div className="btn">
